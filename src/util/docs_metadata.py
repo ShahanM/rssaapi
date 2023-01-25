1535873c75bc4c81079c25cef36a0468a79cb025
Shahan M <iShahanM@gmail.com>
2023-01-25 13:38:12 -0500

    added api descriptions

--- src/util/docs_metadata.py
+++ src/util/docs_metadata.py
@@ -14,42 +14,47 @@ class TagsMetadataEnum(Enum):
 tags_metadata = [
     {
         'name': TagsMetadataEnum.movie.value,
-        'description': ''
-    },
-    {
-        'name': TagsMetadataEnum.ers.value,
-        'description': ''
-    },
-    {
-        'name': TagsMetadataEnum.cybered.value,
-        'description': '',
+        'description': 'This is the movie dataset adapated from the MovieLens dataset of IMDB movies.',
         'externalDocs': {
-            'description': 'Items external docs',
-            'url': 'https://fastapi.tiangolo.com/',
+            'description': 'Movielens Dataset can be found at the MovieLens website.',
+            'url': 'https://grouplens.org/datasets/movielens/',
         }
     },
     {
+        'name': TagsMetadataEnum.ers.value,
+        'description': 'This is the movie dataset with emotional valence ratings from the ERS dataset.'
+    },
+    {
+        'name': TagsMetadataEnum.cybered.value,
+        'description': 'This is a truncated version of the Movie dataset with only movies that contain a TV parential rating of PG-13 or lower.' \
+            + 'This dataset was created for the CyberED project.'
+    },
+    {
         'name': TagsMetadataEnum.user.value,
-        'description': ''
+        'description': 'The user API is used to record study participant responses.'
     },
     {
         'name': TagsMetadataEnum.study.value,
-        'description': ''
+        'description': 'The study API is a study meta API that is used to create and manage the study.' \
+            + 'As such, it is only available to admin users.'
     },
     {
         'name': TagsMetadataEnum.step.value,
-        'description': ''
+        'description': 'The step API is used to create and manage the study steps (e.g. - consent forms, presurveys, preference elicitations).' \
+            + 'It is part of the study API and is only available to admin users.'
     },
 	{
 		'name': TagsMetadataEnum.page.value,
-		'description': ''
+		'description': 'The pages API is used to create multiple pages for a step, for example, the several pages of a survey.' \
+            + 'It is part of the study API and is only available to admin users.'
 	},
 	{
 		'name': TagsMetadataEnum.question.value,
-		'description': ''
+		'description': 'The question API is used to create and manage the survey questions.' \
+            + 'It is part of the study API and is only available to admin users.'
 	},
     {
         'name': TagsMetadataEnum.admin.value,
-        'description': ''
+        'description': 'These are common admin APIs that are used to manage the databases, study metadeta, and contains all the update and delete APIs.'
     }
 ]
