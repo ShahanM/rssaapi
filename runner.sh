3748d5a7f3df748bc4aa40ce2b2b6f979eecdde7
Shahan M Iqbal <mehtabi@clemson.edu>
2025-10-14 13:29:19 -0400

    pyproject.toml and runner.sh updated

--- runner.sh
+++ runner.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/bash
 
-cd src
-uvicorn app:app --reload
+# Ensure we are in the script's directory first, regardless of where it's called from
+cd "$(dirname "$0")" 
+
+export PYTHONPATH=./src:$PYTHONPATH
+# Execute uvicorn from the root, pointing it to src.main:app
+poetry run uvicorn src.main:app --reload
